877f3ac60e28e65918f680e27f314bec25ad7f34
rodbag206 <rbaggs0811@gamil.com>
2025-05-14 19:01:32 -0500

    2nd commit

--- src/main/java/class02/test01.java
+++ src/main/java/class02/test01.java
@@ -4,5 +4,8 @@ public class test01 {
     public static void main(String[] args) {
 
         System.out.println("hello");
+
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
